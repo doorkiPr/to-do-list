945c0ece1d552838ea6017d4730df9b554047140
doorkiPr <boumarafchakib7@gmail.com>
2024-01-07 00:45:38 +0100

    FEAT:add logic that loops through array and create elements based on project data

--- src/modules/RenderProjects.js
+++ src/modules/RenderProjects.js
@@ -1,4 +1,39 @@
 function renderProjects(arrayOfProjects) {
-    const projecstWrapper = document.querySelector(".projectsWrapper");
+    const projectsWrapper = document.querySelector(".projectsWrapper");
 
+    arrayOfProjects.forEach(project => {
+
+        const projectElement = document.createElement("div");
+        projectElement.classList.toggle("project");
+        projectElement.setAttribute("id", project.getId());
+
+        const projectLogo = document.createElement("div");
+        projectLogo.classList.toggle("projectLogo");
+
+        const projectTitle = document.createElement("div");
+        projectTitle.classList.toggle("projectTitle");
+        projectTitle.textContent = project.getTitle();
+
+        const projectButtonWrapper = document.createElement("div");
+        projectButtonWrapper.classList.toggle("projectButtonWrapper");
+
+
+        const editProjectTitle = document.createElement("button");
+        editProjectTitle.classList.toggle("editProjectBtn");
+        editProjectTitle.textContent = "edit";
+
+        const deleteProject = document.createElement("button");
+        deleteProject.classList.toggle("deleteProjectBtn");
+        deleteProject.textContent = "delete";
+
+        projectButtonWrapper.appendChild(editProjectTitle);
+        projectButtonWrapper.appendChild(deleteProject);
+
+        projectElement.appendChild(projectLogo);
+        projectElement.appendChild(projectTitle);
+        projectElement.appendChild(projectButtonWrapper);
+
+        projectsWrapper.appendChild(projectElement);
+
+    });
 }
