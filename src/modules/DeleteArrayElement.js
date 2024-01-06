11faa9e47f68c1fc3ab3df7de5cdf7107610ef33
doorkiPr <boumarafchakib7@gmail.com>
2024-01-06 14:38:40 +0100

    FIX: refractor function to only require array and ID instead of the whole element

--- src/modules/DeleteArrayElement.js
+++ src/modules/DeleteArrayElement.js
@@ -1,8 +1,8 @@
-const deleteElement = (array, elementToDeleted) => {
+const deleteElement = (array, id) => {
 
     array.forEach((element, i) => {
 
-        if (element.id === elementToDeleted.id) {
+        if (element.id === id) {
             array.splice(i, 1)
         }
 
