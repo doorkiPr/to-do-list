42c4e6efb7e670b7aa67770ff954575ffff9d014
doorkiPr <boumarafchakib7@gmail.com>
2024-01-10 03:58:13 +0100

    FEAT:add logic to display and append dialog elements

--- src/modules/RenderTasks.js
+++ src/modules/RenderTasks.js
@@ -30,6 +30,83 @@ function renderTasks(arrayOfTasks) {
         editTask.textContent = "edit";
         deleteTask.textContent = "delete";
 
+        const taskDialog = document.createElement("dialog");
+        const taskForm = document.createElement("form");
+        const taskTitleLabel = document.createElement("label");
+        const taskTitleInput = document.createElement("input");
+        const taskDescLabel = document.createElement("label");
+        const taskDescInput = document.createElement("input");
+        const taskDateLabel = document.createElement("label");
+        const taskDateInput = document.createElement("input");
+        const taskModalBtnWrapper = document.createElement("div");
+        const taskSubmitBtn = document.createElement("button");
+        const taskCancelBtn = document.createElement("button");
+
+        taskDialog.classList.toggle("taskDialog");
+        taskForm.classList.toggle("taskForm");
+        taskTitleLabel.classList.toggle("taskTitleLabel");
+        taskTitleInput.classList.toggle("taskTitleInput");
+        taskDescLabel.classList.toggle("taskDescLabel");
+        taskDescInput.classList.toggle("taskDescInput");
+        taskDateLabel.classList.toggle("taskDateLabel");
+        taskDateInput.classList.toggle("taskDateInput");
+        taskModalBtnWrapper.classList.toggle("taskModalBtnWrapper");
+        taskSubmitBtn.classList.toggle("taskDialogSubmit");
+        taskCancelBtn.classList.toggle("taskDialogCancel");
+
+        taskSubmitBtn.type = "submit";
+        taskForm.method = "dialog";
+
+        taskTitleLabel.htmlFor = "newtaskTitle";
+        taskTitleInput.name = "newtaskTitle";
+        taskTitleInput.setAttribute("id", "newtaskTitle");
+        taskTitleInput.placeholder = "Title ..."
+        taskTitleInput.type = "text";
+        taskTitleInput.required = true;
+        taskTitleInput.maxLength = 15;
+
+        taskTitleLabel.htmlFor = "newtaskDesc";
+        taskTitleInput.name = "newtaskDesc";
+        taskTitleInput.setAttribute("id", "newtaskDesc");
+        taskTitleInput.placeholder = "Description ..."
+        taskTitleInput.type = "text";
+        taskTitleInput.required = true;
+        taskTitleInput.maxLength = 15;
+
+
+        taskTitleLabel.htmlFor = "newtaskDate";
+        taskTitleInput.name = "newtaskDate";
+        taskTitleInput.setAttribute("id", "newtaskDate");
+        taskTitleInput.type = "date";
+        taskTitleInput.required = true;
+
+
+
+
+        taskSubmitBtn.textContent = "Confirm";
+        taskCancelBtn.textContent = "Cancel";
+
+
+        taskForm.addEventListener("submit", () => {
+
+        })
+
+        taskCancelBtn.addEventListener("click", (e) => {
+
+        })
+
+        taskForm.appendChild(taskTitleLabel);
+        taskForm.appendChild(taskTitleInput);
+        taskForm.appendChild(taskDescLabel);
+        taskForm.appendChild(taskDescInput);
+        taskForm.appendChild(taskDateLabel);
+        taskForm.appendChild(taskDateInput);
+        taskModalBtnWrapper.appendChild(taskSubmitBtn);
+        taskModalBtnWrapper.appendChild(taskCancelBtn);
+        taskForm.appendChild(taskModalBtnWrapper);
+
+        taskDialog.appendChild(taskForm);
+
         taskStatus.addEventListener("click", () => {
             // logic that sets the status on the task   
             task.toggleStatus();
