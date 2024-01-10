6d0a8d078578fe74b51ec6336343893f179a81c1
doorkiPr <boumarafchakib7@gmail.com>
2024-01-10 04:39:04 +0100

    FIX:pass arrayoftasks as argument

--- src/modules/RenderTasks.js
+++ src/modules/RenderTasks.js
@@ -116,7 +116,7 @@ function renderTasks(arrayOfTasks) {
         taskStatus.addEventListener("click", () => {
             // logic that sets the status on the task   
             task.toggleStatus();
-            renderTasks();
+            renderTasks(arrayOfTasks);
         });
 
         editTask.addEventListener("click", () => {
