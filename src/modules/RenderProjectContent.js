fb4481886c25711ba8208827c3e56b17ebd2481f
doorkiPr <boumarafchakib7@gmail.com>
2024-01-10 17:39:55 +0100

    FEAT:add textContet for header

--- src/modules/RenderProjectContent.js
+++ src/modules/RenderProjectContent.js
@@ -2,4 +2,6 @@ function renderProjectContent(project) {
     const mainWrapper = document.querySelector(".mainWrapper");
     const mainHeader = document.querySelector(".mainHeader");
 
+    mainHeader.textContent = project.getTitle();
+
 }
