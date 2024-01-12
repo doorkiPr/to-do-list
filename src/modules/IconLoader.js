1353cc1a48a0fc56b629ac819801e5bfb7efbac1
doorkiPr <boumarafchakib7@gmail.com>
2024-01-12 04:32:55 +0100

    FEAT:add logic to add each logo to their respective dom element

--- src/modules/IconLoader.js
+++ src/modules/IconLoader.js
@@ -3,5 +3,17 @@ import week from "../media/week.png";
 import star from "../media/star.png";
 
 function loadIcons() {
+    const allTasksIcon = document.createElement("img");
+    allTasksIcon.src = inbox;
+    document.querySelector("#allTasks").appendChild(allTasksIcon);
+
+    const thisWeekIcon = document.createElement("img");
+    todayIcons.src = week;
+    document.querySelector("#nextDays").appendChild(thisWeekIcon);
+
+    const favoriteIcon = document.createElement("img");
+    favoriteIcon.src = star;
+    
+    document.querySelector("#favorite").appendChild(allTasksIcon);
 
 }
