9ede46ab59bda95bb1447e4c923bab530817c90d
doorkiPr <boumarafchakib7@gmail.com>
2024-01-12 06:48:09 +0100

    FEAT: add logic that adds all tasks from each project to all tasks array '

--- src/modules/defaultProjects/AllTasks.js
+++ src/modules/defaultProjects/AllTasks.js
@@ -1,6 +1,8 @@
 export function allTasks(arrayOfProjects) {
     const allTasksArray = [];
     arrayOfProjects.forEach(element => {
-        
+        allTasksArray.push(...element.getArrayOfTasks());
+        // iiterate through each project and push the tasks inside all task array
     });
+    return allTasksArray
 }
