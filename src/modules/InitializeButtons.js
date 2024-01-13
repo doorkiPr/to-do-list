d518a9ee3901dec465d13fbc367294f5a0fce088
doorkiPr <boumarafchakib7@gmail.com>
2024-01-13 08:24:46 +0100

    FEAT: add logic that switches theme on click

--- src/modules/InitializeButtons.js
+++ src/modules/InitializeButtons.js
@@ -10,6 +10,22 @@ export default function InitializeButtons(arrayOfProjects) {
         renderAddProjectModal(arrayOfProjects);
     });
 
+    document.querySelector("#themeBtn").addEventListener("click", (e) => {
+        const root = document.querySelector(':root');
+        if (e.currentTarget.checked) {
+            root.style.setProperty("--textColor", "white");
+            root.style.setProperty("--bgHeaderFooter", "#22303c");
+            root.style.setProperty("--bgSideBarMain", "#15202b");
+            root.style.setProperty("--bgColor2", "#192734");
+        }
+        else{
+            root.style.setProperty("--textColor", "#374958");
+            root.style.setProperty("--bgHeaderFooter", "#f2f3f5");
+            root.style.setProperty("--bgSideBarMain", "#fefcfe");
+            root.style.setProperty("--bgColor2", "#ececec");
+        };
+
+    });
 }
 
 
