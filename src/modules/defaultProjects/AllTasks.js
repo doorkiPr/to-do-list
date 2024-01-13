2cc276f3ef68f75eaea69d6642685f6c13ec34ef
doorkiPr <boumarafchakib7@gmail.com>
2024-01-13 05:47:53 +0100

    FEAT:itterate through task array and each one into the project's tasks array

--- src/modules/defaultProjects/AllTasks.js
+++ src/modules/defaultProjects/AllTasks.js
@@ -2,10 +2,16 @@ import Project from "../Project";
 
 export function allTasks(arrayOfProjects) {
     const allTasksArray = [];
+    const allTasksProject = Project("All Tasks");
+
     arrayOfProjects.forEach(element => {
         allTasksArray.push(...element.getTasksArray());
         // iiterate through each project and push the tasks inside all task array
     });
-    return allTasksArray
+
+    allTasksArray.forEach(task => {
+        allTasksProject.addTask(task);
+    });
+    return allTasksProject
 }
 /// isntead of just an array this should be a project witht he title All taskks that contains an array of tasks containing every single task 
