6794ec7b2f3394808dcb951db59937a456c645b0
doorkiPr <boumarafchakib7@gmail.com>
2024-01-13 08:47:19 +0100

    FEAT: initialize application

--- src/main.js
+++ src/main.js
@@ -1,50 +1,10 @@
-import Project from "./modules/Project"
-import renderProjects from "./modules/RenderProjects";
-import task from "./modules/Task";
-import css from "./styles/layout.css"
-import inbox from "./media/inbox.png"
+import loadIcons from "./modules/IconLoader";
+import InitializeButtons from "./modules/InitializeButtons";
+import initializeDefaultProjects from "./modules/InitializeDefaultProjects";
+import css from "./styles/layout.css";
 
-const allTasksIcon = document.createElement("img");
-allTasksIcon.src = inbox
-document.querySelector("#allTasks").appendChild(allTasksIcon);
+let projectArray = []; //Initialize array that contains all the projects
 
-
-
-const hiddenMenu = document.querySelector(".menu")
-
-hiddenMenu.addEventListener("click",()=>{
-    document.querySelector(".sidebar").classList.toggle("hidden")
-})
-
-const newProject =  Project("myTitle")
- const secondProject =  Project("someTitle");
-
-const firstTask = task('firsttask','blalablablablabla',new Date('August 19, 1975'))
- const secondTask = task('secondTask','blblblpepepe',new Date('January 12, 2025'))
-
-secondProject.addTask(firstTask)
-secondProject.addTask(secondTask)
-
-const projectArray = [newProject,secondProject];
-
-
-    renderProjects(projectArray)
-
-
-
-// secondProject.setTitle("secondTitle")
-// secondProject.addTask(secondTask)
-
-// console.log(newProject.getTitle()+" first pro");
-// // console.log(secondProject.getTitle()+" second pro");
-
-// newProject.getTasksArray().forEach(task => {
-//     task.toggleStatus();
-//     console.log(task.getStatus());
-// });
-
-// newProject.deleteTask(firstTask.getId())
-
-// newProject.getTasksArray().forEach(task => {
-//     console.log(task);
-// });
+loadIcons();
+InitializeButtons(projectArray);
+initializeDefaultProjects(projectArray);
