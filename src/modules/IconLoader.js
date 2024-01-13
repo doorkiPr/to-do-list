57014ec5b18bfbfdbcd6d503b8053a04920d43e2
doorkiPr <boumarafchakib7@gmail.com>
2024-01-13 08:28:11 +0100

    FEAT: load today icon

--- src/modules/IconLoader.js
+++ src/modules/IconLoader.js
@@ -1,11 +1,16 @@
 import inbox from "../media/inbox.png";
 import week from "../media/week.png";
 import star from "../media/star.png";
+import today from "../media/today.png";
 
 function loadIcons() {
     const allTasksIcon = document.createElement("img");
     allTasksIcon.src = inbox;
     document.querySelector("#allTasks").appendChild(allTasksIcon);
+
+    const todayIcon = document.createElement("img");
+    todayIcon.src = today;
+    document.querySelector("#today").appendChild(todayIcon);
 
     const thisWeekIcon = document.createElement("img");
     thisWeekIcon.src = week;
@@ -13,7 +18,6 @@ function loadIcons() {
 
     const favoriteIcon = document.createElement("img");
     favoriteIcon.src = star;
-    
     document.querySelector("#finished").appendChild(favoriteIcon);
 
 }
