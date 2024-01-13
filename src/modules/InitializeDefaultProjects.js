c1498cb22d99665cce7e17e34da631eaece1bb58
doorkiPr <boumarafchakib7@gmail.com>
2024-01-13 06:35:27 +0100

    FEAT: add today default project

--- src/modules/InitializeDefaultProjects.js
+++ src/modules/InitializeDefaultProjects.js
@@ -1,6 +1,7 @@
 import renderProjectContent from "./RenderProjectContent"
 import { allTasks } from "./defaultProjects/AllTasks"
 import { finishedTasks } from "./defaultProjects/Finished";
+import { todayTasks } from "./defaultProjects/Today";
 
 export default function initializeDefaultProjects(array) {
     renderProjectContent(allTasks(array), true); // show the all Tasks Proejct on load;
@@ -11,5 +12,9 @@ export default function initializeDefaultProjects(array) {
 
     document.querySelector("#finished").addEventListener("click", () => {
         renderProjectContent(finishedTasks(array), true)
+    });
+
+    document.querySelector("#today").addEventListener("click", () => {
+        renderProjectContent(todayTasks(array), true)
     });
 }
